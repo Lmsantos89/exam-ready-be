656433d1efa4f923736db63d23509cc70b3a687d
Lmsantos89 <lmc.santos89@gmail.com>
2025-05-01 09:29:13 +0200

    Add base project commit

---
+++ src/main/java/com/lms/examready/ExamReadyApplication.java
@@ -0,0 +1,13 @@
+package com.lms.examready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamReadyApplication.class, args);
+	}
+
+}
